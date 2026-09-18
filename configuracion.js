21e47909775eb2c78281f876953156fb7bd62fa9
inventario-sanroque <inventario@profe.colegiosanroque.org>
2026-09-18 17:27:38 +0200

    Add files via upload
    Instalación

---
+++ configuracion.js
@@ -0,0 +1,16 @@
+// Configuración del centro · generada con INSTALAR.html el 18/9/2026
+// Estos datos no son secretos: los necesita el navegador para conectar con Firebase.
+window.CONFIG_CENTRO = {
+  "firebase": {
+    "apiKey": "AIzaSyDMNsUoIYrvaP_C9ewHRAGOn7fk_RUjAoE",
+    "authDomain": "robotica-colegio.firebaseapp.com",
+    "projectId": "robotica-colegio",
+    "storageBucket": "robotica-colegio.firebasestorage.app",
+    "messagingSenderId": "713920550550",
+    "appId": "1:713920550550:web:99fb835d758d07d94294b7"
+  },
+  "dominio": "profe.colegiosanroque.org",
+  "proveedor": "google",
+  "tenant": "",
+  "recaptcha": ""
+};
